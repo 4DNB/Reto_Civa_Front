47217158b768ecbdece46aa8958f6ec928d90770
4DNB <davidnb03@outlook.com>
2025-04-02 10:50:58 -0500

    Paginacion

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,25 +2,27 @@ import React, { useEffect, useState } from "react"
 
 export default function Home() {
 
-    const [bus, setBus]=useState([])
-
-    useEffect(()=>{
+    const [bus, setBus] = useState([])
+    const [page, setPage] = useState(0);
+    const [totalPages, setTotalPages] = useState(1); 
+    const limit = 5;
+    useEffect(() => {
         loadBus();
-    },[]);
+    }, [page]);
 
     const loadBus = async () => {
         try {
-            const response = await fetch("http://localhost:8080/bus");
+            const response = await fetch(`http://localhost:8080/bus?page=${page}&size=${limit}`);
             const data = await response.json();
-            console.log(data);
-            setBus(data);
+            setBus(data.content);
+            setTotalPages(data.totalPages);
         } catch (error) {
             console.error("Error al obtener los datos del bus:", error);
         }
     };
 
 
-        
+
     return (
         <div className="container">
             <div className="py-4">
@@ -38,9 +40,9 @@ export default function Home() {
                     </thead>
                     <tbody>
                         {
-                            bus.map((bus,id)=>(
+                            bus.map((bus, id) => (
                                 <tr>
-                                    <th scope="row" key={id}>{id+1}</th>
+                                    <th scope="row" key={id}>{bus.id}</th>
                                     <td>{bus.numBus}</td>
                                     <td>{bus.numPlac}</td>
                                     <td>{bus.fechCre}</td>
@@ -52,6 +54,13 @@ export default function Home() {
                         }
                     </tbody>
                 </table>
+                <button disabled={page === 0} onClick={() => setPage(page - 1)}>
+                    Anterior
+                </button>
+                <span> Página {page + 1} de {totalPages} </span>
+                <button disabled={page >= totalPages - 1} onClick={() => setPage(page + 1)}>
+                    Siguiente
+                </button>
             </div>
         </div>
     )
